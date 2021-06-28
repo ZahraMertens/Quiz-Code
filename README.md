cc574ca74d2ceb44aa2b93bc25c8dc0560ff2dd0
ZahraMertens <zahra.mertens@gmx.net>
2021-06-28 17:20:05 +1000

    Comments

---
+++ README.md
@@ -0,0 +1,66 @@
+### Code-Quiz
+
+## General Information
+
+```
+The deployed Webpage is a code related quiz created with HTML, CSS and JavaScript
+
+URL of my Portfolio: https://zahramertens.github.io/Portfolio-ZahraMertens/
+```
+
+## Table if Contents
+1. [General Information](#general-informaion)
+2. [Task Description](#task-description)
+3. [Changes that have been made](#changes-that-have-been-made)
+4. [Mock-Up](#mock-up)
+5. [Bonus](#bonus)
+6. [Credits](#credits)
+
+
+## Task Describtion
+
+The task was to create a web application from scratch. The webpage is a portfolio which we can add to as the course progresses.
+
+The portfolio is ment to demonstarte our skills and talents of HTML and CSS to employers that might want to hire someone, with a focus on flexbox, media queries and CSS variables. The portfolio demonstrates the highlights of our work and the process behind it to maximize the chances to find a job. The portfolio is ment to align with certain criteria, mentioned below.
+
+**Note:** As I have only created one webpage so far, this is the only one that is linked and the other containers are filled with placeholder images.
+
+## Actual Behaviour
+
+```
+* You are presented with my name, a recent photo, and links to sections about me, my work, and how to contact me.
+
+* When you click on the links in the navigation bar, the UI scrolls to the corresponding section.
+
+* The my-work section includes images of my applications (atm placeholder images) which are titled.
+
+* The first image of the application is larger than the other ones and they all have hover functions.
+
+* When you click on the first/larger image it will take you to the first projects deployed webpage.
+
+* The layout is responsive and adapts to different viewports, such as smartphones and Ipads.
+```
+
+## 🏆 Bonus
+```
+
+* When you click on the Resume PDF element in the navigation bar, you will download my resume.
+
+* When you click on the GitHub or LinkedIn element in the footer, it will lead you too my profile on both social media websites.
+```
+
+## Mock-Up
+
+The following GIF shows the web applications apperance and functionality:
+
+![portfolio-demo](./Assets/images/portfolio.gif)
+
+## Credits
+
+https://www.w3schools.com/html/
+https://css-tricks.com/snippets/css/a-guide-to-flexbox/#flexbox-properties
+https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+https://developer.mozilla.org/en-US/docs/Learn/CSS/Styling_text/Fundamentals
+https://css-tricks.com/snippets/css/style-links-depending-on-destination/
+https://htmlcheatsheet.com/css/
+https://www.w3schools.com/tags/att_a_download.asp
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+//Array with objects defining all questions, answer choices and correct answers
 var myQuestions = [
     {
       question: "What does CSS stand for?",
@@ -27,10 +28,10 @@ var myQuestions = [
     }
   ];
 
-
+//Global variables (static HTML elements)
 var questionContainer = document.querySelector(".question");
 var answerContainer = document.querySelector(".answer");
-var button = document.querySelector(".start-button");
+var buttonStart = document.querySelector(".start-button");
 var timer = document.querySelector(".timer-count");
 var createUlEl = document.querySelector(".ulList");
 var solutionContainer = document.getElementById("solution");
@@ -41,21 +42,51 @@ var body = document.querySelector("body");
 var btnHighscore = document.querySelector(".view-score");
 var headerPEl = document.querySelector(".p-Element");
 
+//Global variables used in more functions
 var timerCount = 60;
 var timePenalty = -10;
 var timerInterval;
-var timeEnd = 0;
 var index = 0;
 var score = 0;
 
+//When start button clicked, button and p element in header gets removed.
+function removeFunction() {
+    
+    buttonStart.remove();
+    headerPEl.remove();
+    
+    //When buttonStart click event is executed, the timer will be set to 60 and renderQuiz() and startTimer() will be executed.
+    timer.textContent = timerCount;
+    renderQuiz();
+    startTimer();
+}
+
+//When timer set to 60 this function will set an interval which so every second the timerCount will be substracted by 1.
+function startTimer() {
+  
+    timerInterval = setInterval(function() {
+        timerCount--;
+        timer.textContent = timerCount;
+
+        //If the timer is 0 the substrction will stop and browser will go to submit form screen
+        if (timerCount === 0){
+            clearInterval(timerInterval);
+            showEndScreen(); 
+            return;
+        }   
+    },1000);
+}
+
+//When the remove function gets executed, the questions and answers will be displayed.
 function renderQuiz () {
 
   questionContainer.setAttribute("class", "question-start");
-  answerContainer.setAttribute("class", "answer-start");
+  answerContainer.setAttribute("class", "answer-start"); //Set Attributes to style container in css
   
   questionContainer.textContent = "";
   createUlEl.textContent = "";
 
+  //Add a for loop to render all questions & the answer choices
   for (var i = 0; i < myQuestions.length; i++){
 
     var addQuestion = myQuestions[index].question;
@@ -63,32 +94,41 @@ function renderQuiz () {
     questionContainer.textContent = addQuestion;
   }
    
+  //Create a new li element for each answer choice
   addChoices.forEach(function (newInput){
     var listEl = document.createElement("li");
     listEl.setAttribute("class", "li-answer")
     listEl.textContent = newInput;
     createUlEl.appendChild(listEl);
-    listEl.addEventListener("click", result);
+    listEl.addEventListener("click", result); //Add event listener, so when any li element is clicked the result function gets executed
   })
 }
  
+//When li element clicked it will check if the answer is correct or wrong
 function result(event){
-  listEl = event.target;
+  listEl = event.target; //define li element as target 
+
   solutionContainer.setAttribute("class", "solution-container")
 
-
+    //If the li element clicked contains the correct answer the score will increase by 1 and the solution...
+    //...container below will contain a text saying that the answer is correct
     if (listEl.textContent == myQuestions[index].correctAnswer){
       score++; 
       solution.textContent = "Correct! The answer is " +  myQuestions[index].correctAnswer;
+
+    //If the li element does not contain the correct answer the timer count will be substracted by 10 seconds...
+    //...and the container below will contain a text saying that the answer is wrong
     } else {
       timerCount = timerCount + timePenalty;
       solution.textContent = "Wrong! The right answer is " +  myQuestions[index].correctAnswer;
     }
 
+    //If we reach the last question the bowser will display the submit form screen
     if (index === 4){
-      timerCount = 0; //stop timer count
+      timerCount = 0; 
       showEndScreen();
       return;
+    //If the is is not the last question, the renderQuiz function will be executed again and the index will be increased by 1 to go through the whole myQuestion array.
     } else {
       index++;
       renderQuiz();
@@ -96,37 +136,14 @@ function result(event){
     }
 }
 
-function removeFunction() {
-    
-    button.remove();
-    headerPEl.remove();
-
-    timer.textContent = timerCount;
-    renderQuiz();
-    startTimer();
-}
-
-function startTimer() {
-  
-    timerInterval = setInterval(function() {
-        timerCount--;
-        timer.textContent = timerCount;
-
-        if (timerCount === 0){
-            clearInterval(timerInterval);
-            showEndScreen(); //stop timer count
-            return;
-        }   
-    },1000);
-
-}
-
+//When the time is up or the last question is answered the browser will display the submit form screen
 function showEndScreen (){
+  //Removing container from before
   header.remove();
   subContainer.remove();
 
+  //Creating new HTML elements
   var pEl = document.createElement("p");
-  pEl.setAttribute("class", "p-Element")
   var formEl = document.createElement("form");
   var labelElName = document.createElement("label");
   var inputElName = document.createElement("input");
@@ -135,8 +152,11 @@ function showEndScreen (){
   var submitEl = document.createElement("input");
   var hEl = document.createElement("h1");
   var main = document.querySelector("main");
-  main.setAttribute("class", "main-El")
-
+ 
+  //Setting attributes and text content for new HTML elements
+  main.setAttribute("class", "main-El");
+  pEl.setAttribute("class", "p-Element");
+  pEl.textContent = "Your final score is: " + score + "/5";
   mainContainer.setAttribute("class", "container-main");
   
   hEl.textContent = "Time is over!";
@@ -164,32 +184,38 @@ function showEndScreen (){
   submitEl.setAttribute("value", "Submit");
   submitEl.setAttribute("class", "submit-btn");
 
-  pEl.textContent = "Your final score is: " + score + "/5";
-
+  //Appending all input, label and button elements to form element
   formEl.appendChild(labelElName);
   formEl.appendChild(inputElName);
   formEl.appendChild(labelElLastName);
   formEl.appendChild(inputElLastName);
   formEl.appendChild(submitEl);
   
+  //Appending new elements to main container
   mainContainer.appendChild(hEl);
   mainContainer.appendChild(pEl);
   mainContainer.appendChild(formEl);
 
-  submitEl.addEventListener("click", saveData);
+  submitEl.addEventListener("click", saveData); //Adding event listener, so when the submit button is clicked a new function gets executed
 }
 
+//When submit button clicked the function gets executed
 function saveData (event) {
     submitEl = event.target;
-    event.preventDefault();
+    event.preventDefault(); //prevent default to reload the page
 
-    var name = document.querySelector(".name-Input").value;
-    var lastName = document.querySelector(".lastName-Input").value;
+    var name = document.querySelector(".name-Input").value; //Selecting data from name input
+    var lastName = document.querySelector(".lastName-Input").value; //Selecting data from last name input
 
+    //If the first name input is empty an elert will pop up
     if (name == "") {
       alert("You must enter a name!")
+
+    //If the last name input is empty an elert will pop up
     } else if (lastName == ""){
       alert("You must enter a last name!")
+
+    //If there is an input in both input fields, the data will be saved as an object
     } else {
       var finalScore = {
         fname: name,
@@ -197,18 +223,22 @@ function saveData (event) {
         scoreData: score
       }
 
+      //Setting local storage key value to save data 
+
       var allScores = localStorage.getItem("allScores");
       if (allScores === null){
-        allScores = [];
+        allScores = []; //If there is no data, it will return an empty array
+
       } else {
-        allScores = JSON.parse(allScores);
+        allScores = JSON.parse(allScores); //If there is a data input the data string will be modified to an object
       }
+
+      //push finalScore object into all scores key value from the local storage
       allScores.push(finalScore);
-      var newScore = JSON.stringify(allScores);
+      var newScore = JSON.stringify(allScores); //all new scores will be modified to a string on order to be able to be saved in the local storage
       localStorage.setItem("allScores", newScore);
 
-      showHighscore();
-
+      showHighscore(); //When button clicked the showHighscores function will get executed
     }
   }
 
@@ -216,28 +246,32 @@ function showHighscore () {
   mainContainer.remove();
   header.remove();
 
+  //Creating new HTML elements for highscore screen
   var highScoreHeader = document.createElement("h1");
-  highScoreHeader.setAttribute("class", "header-highscore")
   var btnWebpage = document.createElement("button");
   var btnClearScore = document.createElement("button");
   var ulElCreate = document.createElement("ul");
-  ulElCreate.setAttribute("class", "ul-score")
 
+  //Setting attributes and text content for new elements 
+  ulElCreate.setAttribute("class", "ul-score");
+  highScoreHeader.setAttribute("class", "header-highscore");
   btnWebpage.setAttribute("type", "button");
   btnWebpage.setAttribute("id", "button-goBack");
   btnWebpage.textContent = "Go Back";
 
   btnClearScore.setAttribute("type", "button");
   btnClearScore.setAttribute("id", "button-clear");
-  btnClearScore.textContent = "Clear Highscores"
 
-  highScoreHeader.textContent = "Last 5 Games"
+  btnClearScore.textContent = "Clear Highscores";
+  highScoreHeader.textContent = "Last Games Scores";
 
+  //Accessing scores from local storage
   var allScores = localStorage.getItem("allScores");
   allScores = JSON.parse(allScores);
 
   document.body.appendChild(highScoreHeader);
 
+  //If allScores has a value, for each new score an li elemnt will be added to the ul element until the local storage gets cleared
     if (allScores !== null){
 
       for (var i = 0; i <allScores.length; i++){
@@ -252,8 +286,8 @@ function showHighscore () {
   document.body.appendChild(btnWebpage);
   document.body.appendChild(btnClearScore);
   
-  btnWebpage.addEventListener("click", showWebpage);
-  btnClearScore.addEventListener("click", clearScore);
+  btnWebpage.addEventListener("click", showWebpage); //goes to the default start of the webpage
+  btnClearScore.addEventListener("click", clearScore); //clears the local storage, so all scores
 }
 
 function clearScore(event){
@@ -266,5 +300,5 @@ function showWebpage(){
   location.reload();
 }
 
-btnHighscore.addEventListener("click", showHighscore);
-button.addEventListener("click", removeFunction);
+btnHighscore.addEventListener("click", showHighscore); //Event to skip quiz and go to the highscore screen
+buttonStart.addEventListener("click", removeFunction); //Event to start the quiz
